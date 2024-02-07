caeebf248e91650cdc754eca555cae48a41f2c25
theMladyPan <stanislav@rubint.sk>
2024-02-07 16:08:39 +0100

    logging update

--- src/Utils/Log.h
+++ src/Utils/Log.h
@@ -29,25 +29,31 @@
 
 #ifdef NDEBUG
 // do not log in release mode
-#define xlog(level, msg) do { } while (0)
+#define xlog(level, msg) \
+    do                   \
+    {                    \
+    } while (0)
 #else
 // define xerxes_log(level, msg) to printf as a macro in format
 // [Time][Log level] — [File]:[Line] [Function] — [Text]
 // e.g. [0.000000][INFO] — [src/main.cpp:123 main] — Hello World!
 // sleep_ms(1) is used to prevent the log messages from being printed in the
 // wrong order, enough for 200 bytes with baudrate 1500000
-#define xlog(level, msg) do { \
-    double timestamp = time_us_64() / 1000000.0f; \
-    std::cout << _CLR_GREY << "[" << timestamp << "][" << _CLR_RST << level << _CLR_GREY << "] — [" << __FILENAME__ << ":" << __LINE__ << " " << __func__ << "()] — " << _CLR_RST << msg << std::endl; \
-    sleep_ms(1); \
-} while (0)
+#define xlog(level, msg)                                                                                                                                                                                   \
+    do                                                                                                                                                                                                     \
+    {                                                                                                                                                                                                      \
+        double timestamp = time_us_64() / 1000000.0f;                                                                                                                                                      \
+        std::cout << _CLR_GREY << "[" << timestamp << "][" << _CLR_RST << level << _CLR_GREY << "] — [" << __FILENAME__ << ":" << __LINE__ << " " << __func__ << "()] — " << _CLR_RST << msg << std::endl; \
+        sleep_ms(10);                                                                                                                                                                                      \
+    } while (0)
 #endif // NDEBUG
 
-
 // always log errors
-#define xlog_error(msg) do { \
-    xlog(_CLR_B_RED << "ERROR" << _CLR_RST, _CLR_RED << msg << _CLR_RST); \
-} while (0)
+#define xlog_error(msg)                                                       \
+    do                                                                        \
+    {                                                                         \
+        xlog(_CLR_B_RED << "ERROR" << _CLR_RST, _CLR_RED << msg << _CLR_RST); \
+    } while (0)
 
 // convenience macros
 #define xlog_err(msg) xlog_error(msg)
@@ -55,24 +61,33 @@
 
 // log warnings if log level is warning or higher
 #if (_LOG_LEVEL >= 2)
-#define xlog_warning(msg) do { \
-    xlog(_CLR_B_YLW << "WARNING" << _CLR_RST, _CLR_YLW << msg << _CLR_RST); \
-} while (0)
+#define xlog_warning(msg)                                                       \
+    do                                                                          \
+    {                                                                           \
+        xlog(_CLR_B_YLW << "WARNING" << _CLR_RST, _CLR_YLW << msg << _CLR_RST); \
+    } while (0)
 #else
-#define xlog_warning(msg) do { } while (0)
+#define xlog_warning(msg) \
+    do                    \
+    {                     \
+    } while (0)
 #endif // _LOG_LEVEL
 
 // convenience macros
 #define xlog_warn(msg) xlog_warning(msg)
 #define xlogw(msg) xlog_warning(msg)
 
-
 #if (_LOG_LEVEL >= 3)
-#define xlog_info(msg) do { \
-    xlog(_CLR_BOLD << "INFO" << _CLR_RST, msg); \
-} while (0)
+#define xlog_info(msg)                              \
+    do                                              \
+    {                                               \
+        xlog(_CLR_BOLD << "INFO" << _CLR_RST, msg); \
+    } while (0)
 #else
-#define xlog_info(msg) do { } while (0)
+#define xlog_info(msg) \
+    do                 \
+    {                  \
+    } while (0)
 #endif // _LOG_LEVEL
 
 // convenience macros
@@ -80,11 +95,16 @@
 
 // log debug messages if log level is debug
 #if (_LOG_LEVEL >= 4)
-#define xlog_debug(msg) do { \
-    xlog(_CLR_B_GREY << "DEBUG" << _CLR_RST, _CLR_GREY << msg << _CLR_RST); \
-} while (0)
+#define xlog_debug(msg)                                                         \
+    do                                                                          \
+    {                                                                           \
+        xlog(_CLR_B_GREY << "DEBUG" << _CLR_RST, _CLR_GREY << msg << _CLR_RST); \
+    } while (0)
 #else
-#define xlog_debug(msg) do { } while (0)
+#define xlog_debug(msg) \
+    do                  \
+    {                   \
+    } while (0)
 #endif // _LOG_LEVEL
 
 // convenience macros
@@ -92,16 +112,20 @@
 #define xlog_dbg(msg) xlog_debug(msg)
 
 #if (_LOG_LEVEL >= 5)
-#define xlog_trace(msg) do { \
-    xlog(_CLR_B_GREY << "TRACE" << _CLR_RST, _CLR_GREY << msg << _CLR_RST); \
-} while (0) 
+#define xlog_trace(msg)                                                         \
+    do                                                                          \
+    {                                                                           \
+        xlog(_CLR_B_GREY << "TRACE" << _CLR_RST, _CLR_GREY << msg << _CLR_RST); \
+    } while (0)
 #else
-#define xlog_trace(msg) do { } while (0)
+#define xlog_trace(msg) \
+    do                  \
+    {                   \
+    } while (0)
 #endif // _LOG_LEVEL
 
 // convenience macros
 #define xlogt(msg) xlog_trace(msg)
 #define xlog_trc(msg) xlog_trace(msg)
 
-
-#endif    /* LOG_HPP */
+#endif /* LOG_HPP */
